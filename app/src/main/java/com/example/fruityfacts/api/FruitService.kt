12f0c02c5f4c4cdb37cf48ae681985d01863b6e8
Kev Carmichael <kev.carmichael22@gmail.com>
2022-04-21 16:42:05 +0100

    added api/FruitService interface

---
+++ app/src/main/java/com/example/fruityfacts/api/FruitService.kt
@@ -0,0 +1,11 @@
+package com.example.fruityfacts.api
+
+import com.example.fruityfacts.data.Fruit
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface FruitService {
+    @GET("{Id}")
+    fun getFruit(@Path("Id") Id: String): Call<Fruit>
+}
