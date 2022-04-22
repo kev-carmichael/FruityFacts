6edb134b5951d5eb7bc2676a337cd90ac044df8b
Kev Carmichael <kev.carmichael22@gmail.com>
2022-04-22 15:25:40 +0100

    removed fruitMap from SingleFruitActivity and FilterResultActivity

--- app/src/main/java/com/example/fruityfacts/FilterResultActivity.kt
+++ app/src/main/java/com/example/fruityfacts/FilterResultActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AlertDialog
 import com.example.fruityfacts.api.FruitService
 import com.example.fruityfacts.api.ServiceBuilder
 import com.example.fruityfacts.data.Fruit
+import com.example.fruityfacts.data.FruitImages
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -17,27 +18,6 @@ class FilterResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_filter_result)
     }
-
-
-    private val fruitMap = mapOf(
-        9 to R.drawable.cherry, 6 to R.drawable.apple,
-        33 to R.drawable.blueberry, 64 to R.drawable.blackberry,
-        35 to R.drawable.apricot, 1 to R.drawable.banana,
-        60 to R.drawable.durian, 68 to R.drawable.fig,
-        69 to R.drawable.gooseberry, 47 to R.drawable.grapes,
-        72 to R.drawable.greenapple, 37 to R.drawable.guava,
-        66 to R.drawable.kiwi, 26 to R.drawable.lemon,
-        44 to R.drawable.lime, 65 to R.drawable.lingonberry,
-        67 to R.drawable.lychee, 27 to R.drawable.mango,
-        41 to R.drawable.melon, 2 to R.drawable.orange,
-        42 to R.drawable.papaya, 70 to R.drawable.passionfruit,
-        4 to R.drawable.pear, 52 to R.drawable.persimmon,
-        10 to R.drawable.pineapple, 71 to R.drawable.plum,
-        23 to R.drawable.raspberry, 3 to R.drawable.strawberry,
-        5 to R.drawable.tomato, 73 to R.drawable.umbu,
-        25 to R.drawable.watermelon
-    )
-
 
 
     private fun loadData(fruitId: String) {
@@ -59,7 +39,7 @@ class FilterResultActivity : AppCompatActivity() {
                     txtName.text = fruitList.find{it.name==fruitId}!!.name
                     txtId.text = fruitList.find{it.name==fruitId}!!.id.toString()
                     txtCalories.text = fruitList.find{it.name==fruitId}!!.nutritions.calories.toString()
-                    fruitMap.get(fruitList.find{it.name==fruitId}!!.id)?.let { imgFruit.setImageResource(it) }
+                    FruitImages().fruitMap.get(fruitList.find{it.name==fruitId}!!.id)?.let { imgFruit.setImageResource(it) }
 
                 }else{
                     //output alert
--- app/src/main/java/com/example/fruityfacts/SingleFruitActivity.kt
+++ app/src/main/java/com/example/fruityfacts/SingleFruitActivity.kt
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AlertDialog
 import com.example.fruityfacts.api.FruitService
 import com.example.fruityfacts.api.ServiceBuilder
 import com.example.fruityfacts.data.Fruit
+import com.example.fruityfacts.data.FruitImages
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -43,26 +44,6 @@ class SingleFruitActivity : AppCompatActivity() {
         const val SEARCH_POSTFIX = " fruit"
     }
 
-
-    private val fruitMap = mapOf(
-        9 to R.drawable.cherry, 6 to R.drawable.apple,
-        33 to R.drawable.blueberry, 64 to R.drawable.blackberry,
-        35 to R.drawable.apricot, 1 to R.drawable.banana,
-        60 to R.drawable.durian, 68 to R.drawable.fig,
-        69 to R.drawable.gooseberry, 47 to R.drawable.grapes,
-        72 to R.drawable.greenapple, 37 to R.drawable.guava,
-        66 to R.drawable.kiwi, 26 to R.drawable.lemon,
-        44 to R.drawable.lime, 65 to R.drawable.lingonberry,
-        67 to R.drawable.lychee, 27 to R.drawable.mango,
-        41 to R.drawable.melon, 2 to R.drawable.orange,
-        42 to R.drawable.papaya, 70 to R.drawable.passionfruit,
-        4 to R.drawable.pear, 52 to R.drawable.persimmon,
-        10 to R.drawable.pineapple, 71 to R.drawable.plum,
-        23 to R.drawable.raspberry, 3 to R.drawable.strawberry,
-        5 to R.drawable.tomato, 73 to R.drawable.umbu,
-        25 to R.drawable.watermelon
-    )
-
     private fun loadData(fruitId: String) {
         val service  = ServiceBuilder.buildService(FruitService::class.java)
         val requestCall = service.getFruit(fruitId)
@@ -83,7 +64,7 @@ class SingleFruitActivity : AppCompatActivity() {
                     txtName.text = fruit.name
                     txtId.text = fruit.id.toString()
                     txtCalories.text = fruit.nutritions.calories.toString()
-                    fruitMap.get(fruit.id)?.let { imgFruit.setImageResource(it) }
+                    FruitImages().fruitMap.get(fruit.id)?.let { imgFruit.setImageResource(it) }
 
                 }else{
                     //output alert
