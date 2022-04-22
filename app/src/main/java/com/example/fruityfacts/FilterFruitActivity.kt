0b4798fe6a35a39f0e86fbae50afc752545b14b4
Kev Carmichael <kev.carmichael22@gmail.com>
2022-04-22 17:44:24 +0100

    added initializeView function to FilterFruitActivity

--- app/src/main/java/com/example/fruityfacts/FilterFruitActivity.kt
+++ app/src/main/java/com/example/fruityfacts/FilterFruitActivity.kt
@@ -1,10 +1,14 @@
 package com.example.fruityfacts
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.slider.RangeSlider
 import com.google.android.material.slider.Slider
 
@@ -13,30 +17,37 @@ class FilterFruitActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_filter_fruit)
 
-        val btnName: Button = findViewById(R.id.btnName)
-        btnName.setOnClickListener {
-            val allFruitIntent = Intent(this, AllFruitActivity::class.java)
-            startActivity(allFruitIntent)
+        initializeView()
+    }
+
+    private fun initializeView() {
+        val sldrCarbohydrates: RangeSlider = findViewById(R.id.sldrCarbohydrates)
+        var values: List<Float> = listOf(0.0f)
+
+        sldrCarbohydrates.addOnSliderTouchListener(object : RangeSlider.OnSliderTouchListener {
+            @SuppressLint("RestrictedApi")
+            override fun onStartTrackingTouch(slider: RangeSlider) {
+                /*val values = sldrCarbohydrates.values
+                println("min=" + values[0])
+                println("max=" + values[1])*/
+            }
+
+            @SuppressLint("RestrictedApi")
+            override fun onStopTrackingTouch(slider: RangeSlider) {
+                values = sldrCarbohydrates.values
+                //println("min=" + values[0])
+                //println("max=" + values[1])
+            }
+        })
+
+        val minValueIntent: Intent = Intent(this,
+            FilterResultActivity::class.java).apply {
+            putExtra("MIN", values[0].toInt())
+
         }
-
-        fun sliderValues() {
-            val sldrCarbohydrates: RangeSlider = findViewById(R.id.sldrCarbohydrates)
-
-            sldrCarbohydrates.addOnSliderTouchListener(object : RangeSlider.OnSliderTouchListener {
-                override fun onStartTrackingTouch(slider: RangeSlider) {
-                    val values = sldrCarbohydrates.values
-                    println("min=" + values[0])
-                    println("max=" + values[1])
-                }
-
-                override fun onStopTrackingTouch(slider: RangeSlider) {
-                    val values = sldrCarbohydrates.values
-                    println("min=" + values[0])
-                    println("max=" + values[1])
-                }
-            })
-        }
-
+        startActivity(minValueIntent)
 
     }
+
+
 }
