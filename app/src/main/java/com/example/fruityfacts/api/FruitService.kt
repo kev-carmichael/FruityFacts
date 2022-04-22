e96f06620fd95fa0147d26c32cc55d6bd891c236
Kev Carmichael <kev.carmichael22@gmail.com>
2022-04-22 12:21:24 +0100

    added getAllFruit function to FruitService

--- app/src/main/java/com/example/fruityfacts/api/FruitService.kt
+++ app/src/main/java/com/example/fruityfacts/api/FruitService.kt
@@ -8,4 +8,8 @@ import retrofit2.http.Path
 interface FruitService {
     @GET("{Id}")
     fun getFruit(@Path("Id") Id: String): Call<Fruit>
+
+    @GET("all")
+    fun getAllFruit(): List<Fruit>
+
 }
