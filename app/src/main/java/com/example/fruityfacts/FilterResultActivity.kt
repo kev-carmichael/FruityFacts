19e47d2907991ef1b9b80dff733df254aaac4950
Kev Carmichael <kev.carmichael22@gmail.com>
2022-04-25 15:43:40 +0100

    added new activity creating FilterResultActivity and activity_filter_result

---
+++ app/src/main/java/com/example/fruityfacts/FilterResultActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fruityfacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FilterResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filter_result)
+    }
+}
