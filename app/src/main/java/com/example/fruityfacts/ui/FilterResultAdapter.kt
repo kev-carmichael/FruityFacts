ada04d700958b3f9689dface81b1c643b534fa7f
Kev Carmichael <kev.carmichael22@gmail.com>
2022-04-25 16:17:42 +0100

    created and added content to FilterResultAdapter

---
+++ app/src/main/java/com/example/fruityfacts/ui/FilterResultAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.fruityfacts.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fruityfacts.R
+import com.example.fruityfacts.data.Fruit
+
+class FilterResultAdapter (private val fruitList: List<Fruit>) :
+    RecyclerView.Adapter<FilterResultAdapter.ViewHolder>() {
+    class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+        val txtName: TextView = view.findViewById(R.id.txtNameRecycler)
+        val txtCalories: TextView = view.findViewById(R.id.txtCaloriesRecycler)
+    }
+
+    override fun getItemCount() = fruitList.size
+
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.fruit_recycler_layout, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val theFruit = fruitList.get(position)
+        holder.txtName.text = "Name: " + theFruit.name
+        holder.txtCalories.text = "Calories: " + theFruit.nutritions.calories.toString()
+        //Picasso.get().load(theCountry
+        //.countryInfo.flag).into(holder.imgFlag)
+    }
+}
