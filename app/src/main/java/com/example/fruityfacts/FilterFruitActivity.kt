c542681251c31a0659bb0939ec6c91a9acaf86cc
Kev Carmichael <kev.carmichael22@gmail.com>
2022-04-25 15:16:40 +0100

    added content to FilterFruitActivity

--- app/src/main/java/com/example/fruityfacts/FilterFruitActivity.kt
+++ app/src/main/java/com/example/fruityfacts/FilterFruitActivity.kt
@@ -1,11 +1,20 @@
 package com.example.fruityfacts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FilterFruitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_filter_fruit)
+
+        /*val btnFindFruit: Button = findViewById(R.id.btnFindFruit)
+        btnFindFruit.setOnClickListener {
+            val intent = Intent(this, FilterResultActivity::class.java)
+            startActivity(intent)
+
+        }*/
     }
 }
