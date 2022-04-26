3bf8d39732f226b0bf2340383545c8b7823d48f7
Kev Carmichael <kev.carmichael22@gmail.com>
2022-04-26 13:38:32 +0100

    added ref in FilterResultAdapter to display fruit photo

--- app/src/main/java/com/example/fruityfacts/ui/FilterResultAdapter.kt
+++ app/src/main/java/com/example/fruityfacts/ui/FilterResultAdapter.kt
@@ -8,11 +8,13 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fruityfacts.R
 import com.example.fruityfacts.data.Fruit
+import com.example.fruityfacts.data.FruitImages
 
 class FilterResultAdapter (private val fruitList: List<Fruit>) :
     RecyclerView.Adapter<FilterResultAdapter.ViewHolder>() {
 
     class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+        val imgFruitRecycler: ImageView = view.findViewById(R.id.imgFruitRecycler)
         val txtNameRecycler: TextView = view.findViewById(R.id.txtNameRecycler)
         val txtIdRecycler: TextView = view.findViewById(R.id.txtIdRecycler)
         val txtCaloriesRecycler: TextView = view.findViewById(R.id.txtCaloriesRecycler)
@@ -28,7 +30,8 @@ class FilterResultAdapter (private val fruitList: List<Fruit>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val theFruit = fruitList.get(position)  //changed from fruitList.get(position)
+        val theFruit = fruitList.get(position)
+        FruitImages().fruitMap.get(theFruit.id)?.let { holder.imgFruitRecycler.setImageResource(it) }
         holder.txtNameRecycler.text = "Name: " + theFruit.name
         holder.txtIdRecycler.text = "Id: " + theFruit.id
         holder.txtCaloriesRecycler.text = "Calories: " + theFruit.nutritions.calories.toString()
