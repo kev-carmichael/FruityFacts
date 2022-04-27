bc1bcc23ec766d41b17424870ca0a015973287d3
Kev Carmichael <kev.carmichael22@gmail.com>
2022-04-27 18:10:44 +0100

    added dark mode to screen 3A

--- app/src/main/java/com/example/fruityfacts/FilterResultActivity.kt
+++ app/src/main/java/com/example/fruityfacts/FilterResultActivity.kt
@@ -1,8 +1,14 @@
 package com.example.fruityfacts
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fruityfacts.api.FruitService
@@ -13,23 +19,58 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class FilterResultActivity : AppCompatActivity() {
+class FilterResultActivity : AppCompatActivity(), SensorEventListener {
+    private lateinit var sensorManager: SensorManager
+    private var light: Sensor? = null
+    private var oldLux: Float = 0F
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_filter_result)
+        //for dark mode
+        sensorManager = getSystemService(Context.SENSOR_SERVICE)
+                as SensorManager
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT) != null) {
+            light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+        } else {
+            // not required
+        }
         loadData()
     }
 
-    //WILL THIS WORK?????????????????????????????
-    val filteredList: MutableList<Fruit> =  ArrayList<Fruit>()
+    override fun onResume() {
+        super.onResume()
+        light?.let { light ->
+            sensorManager.registerListener(this, light,
+                SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
 
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        //not needed, as accuracy is not a factor
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        val lux = event.values[0]
+        if (lux < 20000) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
+
+    val filteredList: MutableList<Fruit> =  ArrayList<Fruit>()
 
     private fun loadData() {
 
         val service  = ServiceBuilder.buildService(FruitService::class.java)
         val requestCall = service.getAllFruit()
-
 
         requestCall.enqueue(object : Callback<List<Fruit>> {
             override fun onResponse(call: Call<List<Fruit>>,
